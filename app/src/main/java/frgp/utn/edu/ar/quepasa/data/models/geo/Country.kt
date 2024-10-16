02538fc13fe1aafd82266deff7cf75c15eaff7aa
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-10-16 16:37:57 -0300

    Limpieza

--- app/src/main/java/frgp/utn/edu/ar/quepasa/data/models/geo/Country.kt
+++
@@ -1,9 +0,0 @@
-package frgp.utn.edu.ar.quepasa.data.models.geo
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Country(
-    val iso3: String,
-    val label: String,
-    val active: Boolean
-)
--- app/src/main/java/frgp/utn/edu/ar/quepasa/view/login/LoginViewModel.kt
+++
@@ -1,8 +0,0 @@
-package frgp.utn.edu.ar.quepasa.view.login
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-
-class LoginViewModel(application: Application): AndroidViewModel(application) {
-
-}

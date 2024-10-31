cdac1f740f6039007c8ef6d6ac6e88b27d6f1719
carlo-renosto <carlorenosto13@gmail.com>
2024-10-31 15:59:48 -0300

    Modularizar TopAppBar

--- app/src/main/java/frgp/utn/edu/ar/quepasa/MainActivity.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/MainActivity.kt
@@ -3,25 +3,9 @@ package frgp.utn.edu.ar.quepasa
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import frgp.utn.edu.ar.quepasa.presentation.activity.auth.AuthenticatedActivity
+import frgp.utn.edu.ar.quepasa.presentation.ui.components.TopMainBar
 import frgp.utn.edu.ar.quepasa.presentation.ui.theme.TP4Theme
 
 class MainActivity : AuthenticatedActivity() {
@@ -38,36 +22,5 @@ class MainActivity : AuthenticatedActivity() {
                 }
             }
         }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TopMainBar(title: String) {
-    Box {
-        TopAppBar(
-            title = {
-                Text(text = title, color = Color.DarkGray)
-            },
-            navigationIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Default.Menu,
-                        contentDescription = "Menu",
-                        tint = Color.DarkGray
-                    )
-                }
-            },
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = Color.LightGray
-            )
-        )
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(2.dp)
-                .background(Color.LightGray)
-                .align(Alignment.BottomCenter)
-        )
     }
 }

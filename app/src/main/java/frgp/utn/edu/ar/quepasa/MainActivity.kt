b800e56a90dc1fb1f97712956ed5563f3f40426d
carlo-renosto <carlorenosto13@gmail.com>
2024-10-31 15:35:59 -0300

    Actualizar MainActivity. Añadir TopAppBar #83

--- app/src/main/java/frgp/utn/edu/ar/quepasa/MainActivity.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/MainActivity.kt
@@ -3,7 +3,24 @@ package frgp.utn.edu.ar.quepasa
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import frgp.utn.edu.ar.quepasa.presentation.activity.auth.AuthenticatedActivity
 import frgp.utn.edu.ar.quepasa.presentation.ui.theme.TP4Theme
 
@@ -14,8 +31,43 @@ class MainActivity : AuthenticatedActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TP4Theme {
-                Text(text = "MainActivity works")
+                Scaffold(
+                    topBar = { TopMainBar(title = "¿Qué pasa?") }
+                ) {
+
+                }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopMainBar(title: String) {
+    Box {
+        TopAppBar(
+            title = {
+                Text(text = title, color = Color.DarkGray)
+            },
+            navigationIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = "Menu",
+                        tint = Color.DarkGray
+                    )
+                }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.LightGray
+            )
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(2.dp)
+                .background(Color.LightGray)
+                .align(Alignment.BottomCenter)
+        )
     }
 }

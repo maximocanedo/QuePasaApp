f573e75e2c30b98b816e9ba2b48cd57468ea0227
carlo-renosto <carlorenosto13@gmail.com>
2024-10-31 15:04:29 -0300

    Crear archivo Theme

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/theme/Theme.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/theme/Theme.kt
@@ -3,7 +3,6 @@ package frgp.utn.edu.ar.quepasa.presentation.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme

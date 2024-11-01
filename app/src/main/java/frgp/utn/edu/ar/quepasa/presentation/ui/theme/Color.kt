80e27636ab712a51b1a9ca5da2de10eb40b00228
carlo-renosto <carlorenosto13@gmail.com>
2024-10-31 21:06:20 -0300

    Actualizar archivo Color

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/theme/Color.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Black1 = Color(0, 8, 20)
+
+val Blue1 = Color(0,29, 61)
+val Blue2 = Color(0, 53, 102)
+
+val Yellow1 = Color(255, 195, 0)
+val Yellow2 = Color(255, 214, 10)

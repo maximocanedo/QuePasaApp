7bd243fa57a08bd9f06677a700f8e8566a0a0019
carlo-renosto <carlorenosto13@gmail.com>
2024-11-02 12:18:55 -0300

    Crear UserProfile

---
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/users/profile/UserProfile.kt
@@ -0,0 +1,16 @@
+package frgp.utn.edu.ar.quepasa.presentation.ui.components.users.profile
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import frgp.utn.edu.ar.quepasa.data.model.User
+import frgp.utn.edu.ar.quepasa.presentation.ui.components.NavigationMainDrawer
+
+@Composable
+fun UserProfile(navController: NavHostController, user: User?) { // TODO: Change to User (non-nullable) after login is implemented
+    Scaffold(
+        topBar = { NavigationMainDrawer(navController, user, "Usuario") }
+    ) {
+
+    }
+}

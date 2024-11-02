93985d9f816bb51d91dca9a1ce1199f85ea26bdf
carlo-renosto <carlorenosto13@gmail.com>
2024-11-02 17:59:56 -0300

    Actualizar UserProfile. Añadir campos #10

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/users/profile/UserProfile.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/users/profile/UserProfile.kt
@@ -1,19 +1,31 @@
 package frgp.utn.edu.ar.quepasa.presentation.ui.components.users.profile
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import frgp.utn.edu.ar.quepasa.data.model.User
 import frgp.utn.edu.ar.quepasa.presentation.ui.components.BaseComponent
+import frgp.utn.edu.ar.quepasa.presentation.ui.components.users.profile.readonly.ReadOnlyUserField
 
 @Composable
 fun UserProfile(navController: NavHostController, user: User?) { // TODO: Change to User (non-nullable) after login is implemented
@@ -22,10 +34,52 @@ fun UserProfile(navController: NavHostController, user: User?) { // TODO: Change
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
-                .border(BorderStroke(2.dp, Color.LightGray))
-                .background(Color.White)
+                .border(BorderStroke(1.dp, Color.LightGray))
         ) {
-            Text(text = "Test")
+            Column(
+                modifier = Modifier
+                    .padding(16.dp
+            )) {
+                Text(text = "Nombre real", fontSize = 18.sp)
+                ReadOnlyUserField(value = "Nombre Ejemplo")
+                Text(text = "Nombre de usuario", fontSize = 18.sp)
+                ReadOnlyUserField(value = "ejemplo-username")
+                Row {
+                    Icon(Icons.Filled.MailOutline, "User Mail")
+                    Text(text = "Correo electrónico", fontSize = 18.sp)
+                }
+                ReadOnlyUserField(value = "ejemplo@mail.com")
+                Row {
+                    Icon(Icons.Filled.Phone, "User Phone")
+                    Text(text = "Teléfono", fontSize = 18.sp)
+                }
+                ReadOnlyUserField(value = "123456789")
+                Row {
+                    Icon(Icons.Filled.Place, "User Place")
+                    Text(text = "Barrio", fontSize = 18.sp)
+                }
+                ReadOnlyUserField(value = "Ejemplo Barrio")
+                
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    IconButton(onClick = { /* TODO: Not implemented yet */ } ) {
+                        Icon(
+                            imageVector = Icons.Filled.Edit,
+                            contentDescription = "User Edit",
+                            tint = Color.White
+                        )
+                    }
+                    IconButton(onClick = { /* TODO: Not implemented yet */ } ) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = "User Delete",
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
         }
     }
 }

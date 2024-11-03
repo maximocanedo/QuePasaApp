6050635a66fbf289973a323862e26e432ed41abd
carlo-renosto <carlorenosto13@gmail.com>
2024-11-02 21:56:12 -0300

    Crear MainNavigationActivity

--- app/src/main/java/frgp/utn/edu/ar/quepasa/MainActivity.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/MainActivity.kt
@@ -1,23 +1,18 @@
 package frgp.utn.edu.ar.quepasa
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.navigation.compose.rememberNavController
+import frgp.utn.edu.ar.quepasa.presentation.activity.MainNavigationActivity
 import frgp.utn.edu.ar.quepasa.presentation.activity.auth.AuthenticatedActivity
-import frgp.utn.edu.ar.quepasa.presentation.ui.components.main.NavigationMainHost
-import frgp.utn.edu.ar.quepasa.presentation.ui.theme.TP4Theme
 
 class MainActivity : AuthenticatedActivity() {
     val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val navController = rememberNavController()
-            TP4Theme {
-                NavigationMainHost(navController = navController, user = getCurrentUser())
-            }
-        }
+        val intent = Intent(this, MainNavigationActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
---
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/activity/MainNavigationActivity.kt
@@ -0,0 +1,20 @@
+package frgp.utn.edu.ar.quepasa.presentation.activity
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import frgp.utn.edu.ar.quepasa.presentation.activity.auth.AuthenticatedActivity
+import frgp.utn.edu.ar.quepasa.presentation.ui.components.main.NavigationMainHost
+import frgp.utn.edu.ar.quepasa.presentation.ui.theme.TP4Theme
+
+class MainNavigationActivity : AuthenticatedActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            TP4Theme {
+                NavigationMainHost(navController = navController, user = getCurrentUser())
+            }
+        }
+    }
+}

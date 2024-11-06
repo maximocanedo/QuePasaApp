ac4fd3a6575a1c730185f1bbc1ab94db7fa519c6
carlo-renosto <carlorenosto13@gmail.com>
2024-11-05 22:01:10 -0300

    Actualizar token

--- app/src/main/java/frgp/utn/edu/ar/quepasa/data/AuthInterceptor.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/data/AuthInterceptor.kt
@@ -6,7 +6,7 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthInterceptor(private val context: Context) : Interceptor {
-    private val defaultToken = "eyJhbGciOiJIUzI1NiJ9.eyJwYXJ0aWFsIjpmYWxzZSwic3ViIjoicm9vdCIsImlhdCI6MTcyODA0OTI0MiwiZXhwIjoyMzM4MDUwNjgyfQ.szj18VgoIbb8RY-N9nUDopXurHr1l7A7KWDom6lBpQY"
+    private val defaultToken = "eyJhbGciOiJIUzI1NiJ9.eyJwYXJ0aWFsIjpmYWxzZSwic3ViIjoicm9vdCIsImlhdCI6MTczMDg0OTU2MywiZXhwIjoyNzMwODUxMDAzfQ.xfCtwmx9KVjQSBH4xw0YclXUeADEg7FoqzDTLMbBArk"
     override fun intercept(chain: Interceptor.Chain): Response {
         val token = getAuthToken(context) ?: defaultToken
         val request = chain.request().newBuilder()

344cea1456cf809fc5ec8a1b94f8211cfdb9af8c
lucas <lucasgruden0@protonmail.com>
2024-11-07 19:06:48 -0300

    Se cambio Picture a Abstract y se creo EventPicture

---
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/data/model/media/EventPicture.kt
@@ -0,0 +1,16 @@
+package frgp.utn.edu.ar.quepasa.data.model.media
+
+import frgp.utn.edu.ar.quepasa.data.model.Event
+import frgp.utn.edu.ar.quepasa.data.model.User
+import java.sql.Timestamp
+import java.util.UUID
+
+data class EventPicture(
+    override var id: UUID,
+    override var description: String,
+    override var active: Boolean,
+    override var mediaType: String,
+    override var uploadedAt: Timestamp,
+    override var owner: User,
+    var event: Event? = null
+) : Picture()
--- app/src/main/java/frgp/utn/edu/ar/quepasa/data/model/media/Picture.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/data/model/media/Picture.kt
@@ -1,16 +1,15 @@
 package frgp.utn.edu.ar.quepasa.data.model.media
 
-import frgp.utn.edu.ar.quepasa.data.dto.response.VoteCount
 import frgp.utn.edu.ar.quepasa.data.model.User
 import java.sql.Timestamp
 import java.util.UUID
 
-data class Picture(
-    val id: UUID,
-    val description: String,
-    val active: Boolean,
-    val mediaType: String,
-    val uploadedAt: Timestamp,
-    val owner: User,
-    val voteCount: VoteCount
-)
+abstract class Picture {
+    abstract var id: UUID
+    abstract var description: String
+    abstract var active: Boolean
+    abstract var mediaType: String
+    abstract var uploadedAt: Timestamp
+    abstract var owner: User
+    //abstract var voteCount: VoteCount
+}

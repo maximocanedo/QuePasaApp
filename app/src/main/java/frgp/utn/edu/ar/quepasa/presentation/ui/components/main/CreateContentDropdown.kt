fb49b6ed8b1a2a0259eb71fba5bad2e5925dd1bb
lucas <lucasgruden0@protonmail.com>
2024-11-08 10:31:03 -0300

    Cambios Dropdown Crear Post/Evento

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/main/CreateContentDropdown.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/main/CreateContentDropdown.kt
@@ -3,13 +3,14 @@ package frgp.utn.edu.ar.quepasa.presentation.ui.components.main
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,7 +19,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -32,12 +32,15 @@ fun CreateContentDropdown(navController : NavHostController) {
         verticalAlignment = Alignment.Bottom,
         horizontalArrangement = Arrangement.End
     ) {
-        IconButton(onClick = { expanded = true }) {
+        FloatingActionButton(
+            onClick = { expanded = true },
+            modifier = Modifier.padding(16.dp),
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+        ) {
             Icon(
-                modifier = Modifier.size(150.dp),
-                imageVector = Icons.Filled.AddCircle,
-                contentDescription = "Post/Event Create",
-                tint = Color.White
+                Icons.Filled.Add,
+                "Post/Event Create",
+                tint = MaterialTheme.colorScheme.primary
             )
         }
 

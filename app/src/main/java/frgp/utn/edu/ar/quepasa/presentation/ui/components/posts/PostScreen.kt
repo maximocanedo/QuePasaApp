a5beeff55c58c431de0b0dfeafb068211a4dbc9a
Patricio_Bordon <patriciobordon123@gmail.com>
2024-11-11 23:34:26 -0300

    Upload

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/posts/PostScreen.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/posts/PostScreen.kt
@@ -1,25 +1,48 @@
 package frgp.utn.edu.ar.quepasa.presentation.ui.components.posts
 
 import PostCard
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import frgp.utn.edu.ar.quepasa.presentation.ui.components.main.TrendsScreen
 import frgp.utn.edu.ar.quepasa.presentation.viewmodel.posts.PostViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import frgp.utn.edu.ar.quepasa.R
 
 @Composable
 fun PostScreen(
-    postViewModel: PostViewModel = hiltViewModel()
+
 ) {
+    val postViewModel: PostViewModel = hiltViewModel()
     val postsState = postViewModel.posts.collectAsStateWithLifecycle()
 
-    Column {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "Tendencias",
+            modifier = Modifier.padding(16.dp),
+            color = Color.Gray,
+            fontSize = 20.sp
+        )
+        TrendsScreen()
+
+        Text(
+            text = "Publicaciones",
+            modifier = Modifier.padding(16.dp),
+            color = Color.Gray,
+            fontSize = 20.sp
+        )
         postsState.value.content.forEach { post ->
             PostCard(
                 post = post,
@@ -39,6 +62,7 @@ fun PostScreen(
                 }
             )
         }
-       
     }
+
+
 }

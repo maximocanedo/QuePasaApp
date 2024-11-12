8155e97111b8fb0011c3c53d2230af1a62d61b98
lucas <lucasgruden0@protonmail.com>
2024-11-12 10:16:51 -0300

    EventPictureRepository y EventPictureService

---
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/data/source/remote/media/EventPictureService.kt
@@ -0,0 +1,43 @@
+package frgp.utn.edu.ar.quepasa.data.source.remote.media
+
+import frgp.utn.edu.ar.quepasa.data.model.media.EventPicture
+import frgp.utn.edu.ar.quepasa.utils.pagination.Page
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Path
+import retrofit2.http.Query
+import java.util.UUID
+
+interface EventPictureService {
+    @Multipart
+    @POST("event-pictures")
+    suspend fun upload(
+        @Part file: MultipartBody.Part,
+        @Part("event") event: RequestBody,
+        @Part("description") description: RequestBody
+    ): Response<EventPicture>
+
+    @GET("event-pictures/{id}")
+    suspend fun getPictureById(@Path("id") id: UUID): Response<EventPicture>
+
+    @GET("event-pictures/{id}/view")
+    suspend fun viewPicture(@Path("id") id: UUID): Response<ResponseBody>
+
+    @GET("event-pictures/event/{id}")
+    suspend fun getPicturesByEvent(
+        @Path("id") id: UUID,
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): Response<Page<EventPicture>>
+
+    @DELETE("event-pictures/{id}")
+    suspend fun deletePicture(@Path("id") id: UUID): Response<Void>
+
+}
---
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/domain/repository/media/EventPictureRepository.kt
@@ -0,0 +1,50 @@
+package frgp.utn.edu.ar.quepasa.domain.repository.media
+
+import frgp.utn.edu.ar.quepasa.data.model.media.EventPicture
+import frgp.utn.edu.ar.quepasa.data.source.remote.media.EventPictureService
+import frgp.utn.edu.ar.quepasa.utils.pagination.Page
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.ResponseBody
+import retrofit2.Response
+import java.io.File
+import java.util.UUID
+import javax.inject.Inject
+
+class EventPictureRepository @Inject constructor(
+    private val pictureService: EventPictureService
+) {
+    private suspend fun <T> handleResponse(call: suspend () -> Response<T>): T & Any {
+        val response = call.invoke()
+        if (response.isSuccessful) {
+            return response.body() ?: throw Exception("Empty body")
+        } else {
+            throw Exception("Error en la respuesta: ${response.errorBody()}")
+        }
+    }
+
+    suspend fun getPictureById(id: UUID): EventPicture =
+        handleResponse { pictureService.getPictureById(id) }
+
+    suspend fun viewPicture(id: UUID): ResponseBody =
+        handleResponse { pictureService.viewPicture(id) }
+
+
+    suspend fun getPicturesByEvent(id: UUID, page: Int, size: Int): Page<EventPicture> =
+        handleResponse { pictureService.getPicturesByEvent(id, page, size) }
+
+    suspend fun upload(file: File, event: UUID, description: String): EventPicture? {
+        val fileRequestBody = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+        val filePart = MultipartBody.Part.createFormData("file", file.name, fileRequestBody)
+
+        val eventRequestBody = event.toString().toRequestBody("text/plain".toMediaTypeOrNull())
+        val descriptionRequestBody = description.toRequestBody("text/plain".toMediaTypeOrNull())
+
+        return pictureService.upload(filePart, eventRequestBody, descriptionRequestBody).body()
+    }
+
+    suspend fun deletePicture(id: UUID): Void =
+        handleResponse { pictureService.deletePicture(id) }
+}

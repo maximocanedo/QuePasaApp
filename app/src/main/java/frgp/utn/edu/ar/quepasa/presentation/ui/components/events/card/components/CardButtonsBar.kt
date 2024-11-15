4ed1e9404d428a902c535f1ba9c5af1e4329ce5f
lucas <lucasgruden0@protonmail.com>
2024-11-15 17:36:36 -0300

    Color a manito al marcar asistencia

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/events/card/components/CardButtonsBar.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/events/card/components/CardButtonsBar.kt
@@ -5,7 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -26,6 +31,7 @@ fun CardButtonsBar(
     onUpvoteClick: () -> Unit,
     onDownvoteClick: () -> Unit
 ) {
+    var assist by remember { mutableStateOf(false) }
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -39,6 +45,12 @@ fun CardButtonsBar(
                 R.drawable.baseline_back_hand_24,
                 onClick = {
                     onAssistanceClick()
+                    assist = !assist
+                },
+                tint = if (assist) {
+                    MaterialTheme.colorScheme.inversePrimary
+                } else {
+                    MaterialTheme.colorScheme.onPrimary
                 }
             )
         }

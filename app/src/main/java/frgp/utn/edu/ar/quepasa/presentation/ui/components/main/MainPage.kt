3d174f23bb980f8591d61daf0037698a9b78b119
Patricio_Bordon <patriciobordon123@gmail.com>
2024-11-16 03:21:07 -0300

    Update MainPage.kt

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/main/MainPage.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/main/MainPage.kt
@@ -37,6 +37,9 @@ fun MainPage(navController: NavHostController) { // TODO: Change to User (non-nu
             if (user.user?.role == Role.USER) {
                 WarningMessage("Tu cuenta se encuentra sin verificar. Para acceder a todas las funcionalidades, verifica tu cuenta en el apartado de usuarios.")
             }
+            Box(modifier = Modifier.padding(16.dp)) {
+                Text(text = "Bienvenido, ${if (user.ok) user.name else "Usuario"}")
+            }
 
             PostScreen(navController, selectedTag = null)
         }

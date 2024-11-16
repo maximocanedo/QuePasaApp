92b3465c809573ef60e28a348673a85159f729e7
carlo-renosto <carlorenosto13@gmail.com>
2024-11-16 06:56:33 -0300

    Pantallas scrolleables

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/events/CreateEventScreen.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/events/CreateEventScreen.kt
@@ -70,153 +70,155 @@ fun CreateEventScreen(navController: NavHostController) {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Row {
-                TitleField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = title,
-                    validator = viewModel::titleValidator,
-                    onChange = { value, valid ->
+            androidx.compose.foundation.rememberScrollState().let { scrollState ->
+                Row {
+                    TitleField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = title,
+                        validator = viewModel::titleValidator,
+                        onChange = { value, valid ->
                             viewModel.setTitle(value)
                             viewModel.setTitleIsValid(valid)
-                    }
-                )
-            }
-            Row {
-                DescriptionField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = description,
-                    validator = viewModel::descriptionValidator,
-                    onChange = { value, valid ->
+                        }
+                    )
+                }
+                Row {
+                    DescriptionField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = description,
+                        validator = viewModel::descriptionValidator,
+                        onChange = { value, valid ->
                             viewModel.setDescription(value)
                             viewModel.setDescriptionIsValid(valid)
-                    }
-                )
-            }
-            Row {
-                AddressField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = address,
-                    validator = viewModel::addressValidator,
-                    onChange = { value, valid ->
+                        }
+                    )
+                }
+                Row {
+                    AddressField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = address,
+                        validator = viewModel::addressValidator,
+                        onChange = { value, valid ->
                             viewModel.setAddress(value)
                             viewModel.setAddressIsValid(valid)
-                    }
-                )
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Column(
-                    modifier = Modifier.weight(1f)
+                        }
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
-                    DateField(
-                        modifier = Modifier.fillMaxWidth(),
-                        value = start,
-                        validator = { viewModel.startValidator(it) },
-                        onChange = { value, valid ->
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        DateField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = start,
+                            validator = { viewModel.startValidator(it) },
+                            onChange = { value, valid ->
                                 viewModel.setStart(value)
                                 viewModel.setStartDateIsValid(valid)
-                            viewModel.endDateValidation()
-                        },
-                        label = "Fecha de inicio",
-                        isError = !viewModel.startDateIsValid.collectAsState().value,
-                        errorMessage = viewModel.startDateErrorMessage,
-                    )
-                }
-                Column(
-                    modifier = Modifier.weight(1f)
-                ) {
-                    DateField(
-                        modifier = Modifier.widthIn(120.dp, 240.dp),
-                        value = end,
-                        validator = { viewModel.endValidator(it) },
-                        onChange = { value, valid ->
+                                viewModel.endDateValidation()
+                            },
+                            label = "Fecha de inicio",
+                            isError = !viewModel.startDateIsValid.collectAsState().value,
+                            errorMessage = viewModel.startDateErrorMessage,
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        DateField(
+                            modifier = Modifier.widthIn(120.dp, 240.dp),
+                            value = end,
+                            validator = { viewModel.endValidator(it) },
+                            onChange = { value, valid ->
                                 viewModel.setEnd(value)
                                 viewModel.setEndDateIsValid(valid)
-                            viewModel.startDateValidation()
-                        },
-                        label = "Fecha de fin",
-                        isError = !viewModel.endDateIsValid.collectAsState().value,
-                        errorMessage = viewModel.endDateErrorMessage,
-                    )
-                }
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Column(
-                    modifier = Modifier.weight(1f)
-                ) {
-                    EventAudienceField(
-                        modifier = Modifier
-                            .padding(bottom = 8.dp),
-                        audience = audience,
-                        onItemSelected = {
-                            viewModel.setAudience(it)
-                        }
-                    )
-                }
-                Column(
-                    modifier = Modifier.weight(1f)
-                ) {
-                    EventCategoryField(
-                        modifier = Modifier
-                            .padding(bottom = 8.dp),
-                        category = category,
-                        onItemSelected = {
-                            viewModel.setCategory(it)
-                        }
-                    )
-                }
-            }
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Column(
-                    modifier = Modifier.weight(0.7f)
-                ) {
-                    OutlinedTextField(
-                        value = neighbourhoodsNames.joinToString(", "),
-                        onValueChange = {},
-                        readOnly = true,
-                        label = { Text("Barrios") },
-                        modifier = Modifier.fillMaxWidth(),
-                        isError = !viewModel.neighbourhoodIsValid.collectAsState().value,
-                        supportingText = {
-                            if (!viewModel.neighbourhoodIsValid.collectAsState().value) {
-                                Text("Debe seleccionar al menos un barrio")
-                            }
-                        },
-                    )
-                }
-                Column(
-                    modifier = Modifier.weight(0.3f)
-                ) {
-                    Button(
-                        onClick = {
-                            openNeighbourhoodDialog.value = true
-                        },
-                        shape = MaterialTheme.shapes.medium
-                    ) {
-                        Text("Agregar Barrios")
+                                viewModel.startDateValidation()
+                            },
+                            label = "Fecha de fin",
+                            isError = !viewModel.endDateIsValid.collectAsState().value,
+                            errorMessage = viewModel.endDateErrorMessage,
+                        )
                     }
                 }
-            }
-            Row {
-                EventCreateImagesPreview(
-                    modifier = Modifier,
-                    imageViewModel.selectedUris,
-                    imageViewModel::clearImage
-                )
-                EventImageField(
+                Row(
                     modifier = Modifier.fillMaxWidth(),
-                    imageViewModel::addImages
-                )
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        EventAudienceField(
+                            modifier = Modifier
+                                .padding(bottom = 8.dp),
+                            audience = audience,
+                            onItemSelected = {
+                                viewModel.setAudience(it)
+                            }
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        EventCategoryField(
+                            modifier = Modifier
+                                .padding(bottom = 8.dp),
+                            category = category,
+                            onItemSelected = {
+                                viewModel.setCategory(it)
+                            }
+                        )
+                    }
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(
+                        modifier = Modifier.weight(0.7f)
+                    ) {
+                        OutlinedTextField(
+                            value = neighbourhoodsNames.joinToString(", "),
+                            onValueChange = {},
+                            readOnly = true,
+                            label = { Text("Barrios") },
+                            modifier = Modifier.fillMaxWidth(),
+                            isError = !viewModel.neighbourhoodIsValid.collectAsState().value,
+                            supportingText = {
+                                if (!viewModel.neighbourhoodIsValid.collectAsState().value) {
+                                    Text("Debe seleccionar al menos un barrio")
+                                }
+                            },
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.weight(0.3f)
+                    ) {
+                        Button(
+                            onClick = {
+                                openNeighbourhoodDialog.value = true
+                            },
+                            shape = MaterialTheme.shapes.medium
+                        ) {
+                            Text("Agregar Barrios")
+                        }
+                    }
+                }
+                Row {
+                    EventCreateImagesPreview(
+                        modifier = Modifier,
+                        imageViewModel.selectedUris,
+                        imageViewModel::clearImage
+                    )
+                    EventImageField(
+                        modifier = Modifier.fillMaxWidth(),
+                        imageViewModel::addImages
+                    )
+                }
             }
             Row {
                 Button(
--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/posts/PostCreateScreen.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/posts/PostCreateScreen.kt
@@ -74,105 +74,107 @@ fun PostCreateScreen(navController: NavHostController, user: User?) {
         val tags by viewModel.tags.collectAsState()
 
         Column(modifier = Modifier.padding(16.dp)) {
-            Row(modifier = Modifier.fillMaxWidth()) {
-                TypeField(
-                    modifier = Modifier.weight(1f),
-                    typeViewModel,
-                    subtype = subtype,
-                    loadBySubtype = false,
-                    onItemSelected = {
-                        type = it
-                    }
-                )
-                Spacer(modifier = Modifier.width(4.dp))
-                TypeSubtypeField(
-                    modifier = Modifier.weight(1f),
-                    viewModel = subtypeViewModel,
-                    type = type,
-                    subtype = subtype,
-                    loadBySelected = false,
-                    onItemSelected = { subtype = it }
-                )
-            }
-            Spacer(modifier = Modifier.height(20.dp))
-            Row(modifier = Modifier.fillMaxWidth()) {
-                AudienceField(
-                    modifier = Modifier.weight(1f),
-                    audience = audience,
-                    onItemSelected = { audience = it }
-                )
-                Spacer(modifier = Modifier.width(4.dp))
-                NeighbourhoodField(
-                    modifier = Modifier.weight(1f),
-                    audience = audience,
-                    neighbourhood = neighbourhood,
-                    loadBySelected = false,
-                    onItemSelected = { neighbourhood = it }
-                )
-            }
+            androidx.compose.foundation.rememberScrollState().let { scrollState ->
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    TypeField(
+                        modifier = Modifier.weight(1f),
+                        typeViewModel,
+                        subtype = subtype,
+                        loadBySubtype = false,
+                        onItemSelected = {
+                            type = it
+                        }
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    TypeSubtypeField(
+                        modifier = Modifier.weight(1f),
+                        viewModel = subtypeViewModel,
+                        type = type,
+                        subtype = subtype,
+                        loadBySelected = false,
+                        onItemSelected = { subtype = it }
+                    )
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    AudienceField(
+                        modifier = Modifier.weight(1f),
+                        audience = audience,
+                        onItemSelected = { audience = it }
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    NeighbourhoodField(
+                        modifier = Modifier.weight(1f),
+                        audience = audience,
+                        neighbourhood = neighbourhood,
+                        loadBySelected = false,
+                        onItemSelected = { neighbourhood = it }
+                    )
+                }
 
-            Spacer(modifier = Modifier.height(20.dp))
-            TagField(
-                modifier = Modifier.padding(2.dp),
-                value = tag,
-                validator = {
-                    StringValidator(tag)
-                        .isNotBlank()
-                        .hasMaximumLength(15)
-                        .hasMinimumLength(3)
-                },
-                onChange = { newTags -> tag = newTags },
-                onValidityChange = { status -> viewModel.toggleTagValidationField(status) },
-                onAdded = { tag = "" },
-                viewModel = viewModel
-            )
+                Spacer(modifier = Modifier.height(20.dp))
+                TagField(
+                    modifier = Modifier.padding(2.dp),
+                    value = tag,
+                    validator = {
+                        StringValidator(tag)
+                            .isNotBlank()
+                            .hasMaximumLength(15)
+                            .hasMinimumLength(3)
+                    },
+                    onChange = { newTags -> tag = newTags },
+                    onValidityChange = { status -> viewModel.toggleTagValidationField(status) },
+                    onAdded = { tag = "" },
+                    viewModel = viewModel
+                )
 
-            if(tags.isNotEmpty()) {
-                FlowRow(modifier = Modifier.fillMaxWidth()) {
-                    tags.forEachIndexed { index, tag ->
-                        TagValue(
-                            modifier = Modifier
-                                .weight(1f),
-                            value = tag,
-                            viewModel = viewModel
-                        )
+                if (tags.isNotEmpty()) {
+                    FlowRow(modifier = Modifier.fillMaxWidth()) {
+                        tags.forEachIndexed { index, tag ->
+                            TagValue(
+                                modifier = Modifier
+                                    .weight(1f),
+                                value = tag,
+                                viewModel = viewModel
+                            )
 
-                        if(index < tags.size - 1) {
-                            Spacer(modifier = Modifier.width(4.dp))
+                            if (index < tags.size - 1) {
+                                Spacer(modifier = Modifier.width(4.dp))
+                            }
                         }
                     }
                 }
-            }
 
-            Spacer(modifier = Modifier.height(10.dp))
-            TitleField(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                value = title,
-                validator = {
-                    StringValidator(title)
-                        .isNotBlank()
-                        .hasMaximumLength(30)
-                        .hasMinimumLength(4)
-                },
-                onChange = { newTitle -> title = newTitle },
-                onValidityChange = { status -> viewModel.toggleValidationField(0, status) }
-            )
-            DescriptionField(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                value = description,
-                validator = {
-                    StringValidator(description)
-                        .isNotBlank()
-                        .hasMaximumLength(256)
-                        .hasMinimumLength(4)
-                },
-                onChange = { newDesc -> description = newDesc },
-                onValidityChange = { status -> viewModel.toggleValidationField(1, status) }
-            )
-            PostCreateImagesPreview(modifier = Modifier, viewModel = imageViewModel)
-            ImageField(modifier = Modifier.fillMaxWidth(), viewModel = imageViewModel)
+                Spacer(modifier = Modifier.height(10.dp))
+                TitleField(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    value = title,
+                    validator = {
+                        StringValidator(title)
+                            .isNotBlank()
+                            .hasMaximumLength(30)
+                            .hasMinimumLength(4)
+                    },
+                    onChange = { newTitle -> title = newTitle },
+                    onValidityChange = { status -> viewModel.toggleValidationField(0, status) }
+                )
+                DescriptionField(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    value = description,
+                    validator = {
+                        StringValidator(description)
+                            .isNotBlank()
+                            .hasMaximumLength(256)
+                            .hasMinimumLength(4)
+                    },
+                    onChange = { newDesc -> description = newDesc },
+                    onValidityChange = { status -> viewModel.toggleValidationField(1, status) }
+                )
+                PostCreateImagesPreview(modifier = Modifier, viewModel = imageViewModel)
+                ImageField(modifier = Modifier.fillMaxWidth(), viewModel = imageViewModel)
+            }
 
             Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
                 Button(onClick = {

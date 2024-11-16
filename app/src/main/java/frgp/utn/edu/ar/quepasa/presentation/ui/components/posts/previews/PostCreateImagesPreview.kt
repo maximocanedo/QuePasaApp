38a9bf422d7ae0fa82fc73a526c7ec1c03db5aea
carlo-renosto <carlorenosto13@gmail.com>
2024-11-16 01:49:28 -0300

    Aplicar Crop a imágenes

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/posts/previews/PostCreateImagesPreview.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/posts/previews/PostCreateImagesPreview.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.rememberAsyncImagePainter
 import frgp.utn.edu.ar.quepasa.presentation.viewmodel.images.ImageViewModel
@@ -37,7 +38,8 @@ fun PostCreateImagesPreview(modifier: Modifier, viewModel: ImageViewModel) {
                     Image(
                         painter = rememberAsyncImagePainter(model = "content://media${uri.path}"),
                         contentDescription = "Image Preview",
-                        modifier = Modifier.matchParentSize()
+                        modifier = Modifier.matchParentSize(),
+                        contentScale = ContentScale.Crop
                     )
 
                     IconButton(

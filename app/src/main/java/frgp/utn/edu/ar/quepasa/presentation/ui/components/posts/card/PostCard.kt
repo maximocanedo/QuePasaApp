3dd32231e10b8fd4d5df20baf49f096d10fd13a2
lucas <lucasgruden0@protonmail.com>
2024-11-16 02:09:36 -0300

    Titulo card posts

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/posts/card/PostCard.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/posts/card/PostCard.kt
@@ -1,7 +1,20 @@
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -91,8 +104,18 @@ fun PostCard(
                     )
                 }
             }
-
             Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = post.title,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            HorizontalDivider(
+                modifier = Modifier.padding(vertical = 8.dp),
+                thickness = 2.dp,
+                color = MaterialTheme.colorScheme.secondary
+            )
 
             ReadMoreText(
                 text = post.description,

5c4291b2b779a7b2207b03e0bc83a056c8f5b48c
lucas <lucasgruden0@protonmail.com>
2024-11-19 10:11:35 -0300

    Implementacion Comentarios

--- app/src/main/java/frgp/utn/edu/ar/quepasa/data/source/remote/commenting/CommentService.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/data/source/remote/commenting/CommentService.kt
@@ -1,9 +1,7 @@
 package frgp.utn.edu.ar.quepasa.data.source.remote.commenting
 
+import frgp.utn.edu.ar.quepasa.data.dto.response.VoteCount
 import frgp.utn.edu.ar.quepasa.data.model.Comment
-import frgp.utn.edu.ar.quepasa.data.model.commenting.EventComment
-import frgp.utn.edu.ar.quepasa.data.model.commenting.PostComment
-import frgp.utn.edu.ar.quepasa.utils.pagination.Page
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -11,28 +9,24 @@ import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
-import retrofit2.http.Query
 import java.util.UUID
 
 interface CommentService {
-    @POST("/api/posts/{id}/comments")
-    suspend fun createPostComment(@Path("id") postId: Int, @Body file: String): Response<Comment>
+    @POST("/comments/{commentId}/votes/up")
+    suspend fun upVoteComment(@Path("commentId") commentId: UUID): Response<VoteCount>
 
-    @POST("/api/events/{id}/comments")
-    suspend fun createEventComment(@Path("id") eventId: UUID, @Body file: String): Response<Comment>
+    @POST("/comments/{commentId}/votes/down")
+    suspend fun downVoteComment(@Path("commentId") commentId: UUID): Response<VoteCount>
 
-    @GET("/api/posts/{id}/comments")
-    suspend fun getCommentsByPost(@Path("id") id: Int, @Query("page") page: Int, @Query("size") size: Int): Response<Page<PostComment>>
+    @GET("/comments/{commentId}")
+    suspend fun getCommentById(@Path("commentId") commentId: UUID): Response<Comment>
 
-    @GET("/api/events/{id}/comments")
-    suspend fun getCommentsByEvent(@Path("id") id: UUID, @Query("page") page: Int, @Query("size") size: Int): Response<Page<EventComment>>
+    @PATCH("/comments/{commentId}")
+    suspend fun updateComment(
+        @Path("commentId") commentId: UUID,
+        @Body content: String
+    ): Response<Comment>
 
-    @GET("/comments/{id}")
-    suspend fun getCommentById(@Path("id") id: UUID): Response<Comment>
-
-    @PATCH("/comments/{id}")
-    suspend fun updateComment(@Path("id") id: UUID, @Body content: String): Response<Comment>
-
-    @DELETE("/comments/{id}")
-    suspend fun deleteComment(@Path("id") id: UUID): Response<Void>
+    @DELETE("/comments/{commentId}")
+    suspend fun deleteComment(@Path("commentId") commentId: UUID): Response<Void>
 }

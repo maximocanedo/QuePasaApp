4c2e575370725cfc4c7c374552769e63dcd0e4cd
Patricio_Bordon <patriciobordon123@gmail.com>
2024-11-20 00:38:15 -0300

    algoritmo trends por barrio

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/main/TrendsScreen.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/main/TrendsScreen.kt
@@ -7,12 +7,18 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import frgp.utn.edu.ar.quepasa.presentation.ui.components.main.TrendsCarousel
 import frgp.utn.edu.ar.quepasa.presentation.viewmodel.trends.TrendsViewModel
 
+import androidx.compose.runtime.LaunchedEffect
+import frgp.utn.edu.ar.quepasa.domain.context.user.AuthenticationContext
+
 @Composable
-fun TrendsScreen(navController: NavHostController) {
+fun TrendsScreen(navController: NavHostController, user: AuthenticationContext) {
     val trendsViewModel: TrendsViewModel = hiltViewModel()
+
+    LaunchedEffect(user) {
+        trendsViewModel.setUser(user)
+    }
 
     Column(modifier = Modifier.padding(12.dp)) {
         TrendsCarousel(

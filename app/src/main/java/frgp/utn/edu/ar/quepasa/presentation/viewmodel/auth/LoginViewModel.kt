a2d2eb942c78e6fc0ebf42c33f95c7f121bfa75e
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-11-22 09:19:04 -0300

    Solucionar error al iniciar sesión con TOTP

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/viewmodel/auth/LoginViewModel.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/viewmodel/auth/LoginViewModel.kt
@@ -231,15 +231,6 @@ class LoginViewModel @Inject constructor(
 
         when (res) {
             is ApiResponse.Success -> {
-                val user = userRepository.getAuthenticatedUser()
-                if (user == null) {
-                    snackMutable.emit("Usuario no encontrado o hubo un error.")
-                    return
-                }
-                if (!user.active) {
-                    snackMutable.emit("Tu cuenta no está activa. Contacta al soporte.")
-                    return
-                }
                 requiresTOTPMutable.value = res.data?.totpRequired ?: false
                 if (!requiresTOTPMutable.value) {
                     updateLoggedInState(true)
@@ -254,7 +245,7 @@ class LoginViewModel @Inject constructor(
                 if (requiresTOTPMutable.value) {
                     invalidateTotp()
                 } else {
-                    snackMutable.emit("Error: ${res.exception.message}")
+                    snackMutable.emit("Error: ${res.exception.status ?: 0}")
                 }
             }
         }

d5c742598973dc072c3f6f4a6de3f42cc1b6b508
lucas <lucasgruden0@protonmail.com>
2024-11-22 22:55:02 -0300

    Backroute eventos arreglada

--- app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/events/EditEventScreen.kt
+++ app/src/main/java/frgp/utn/edu/ar/quepasa/presentation/ui/components/events/EditEventScreen.kt
@@ -66,7 +66,7 @@ fun EditEventScreen(navController: NavHostController, eventId: UUID) {
         pictureViewModel.setPicturesBitmap(eventPictureViewModel.picturesIds.value)
     }
     if (event != null) {
-        BaseComponent(navController, "Editar de Evento", true) {
+        BaseComponent(navController, "Editar de Evento", true, backRoute = "events") {
             val title by viewModel.title.collectAsState()
             val description by viewModel.description.collectAsState()
             val address by viewModel.address.collectAsState()
